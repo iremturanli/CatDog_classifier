574e0d7ecf8d0f5c24c9710a4b9c2bcd5400b465
= <iremturanli@hotmail.com>
2021-08-20 17:30:19 +0300

    ilk

--- cat_detector.py
+++ cat_detector.py
@@ -2,12 +2,12 @@ import argparse
 import cv2
 import os
 import numpy as np
-
+from PIL import Image as im
 
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
+ap.add_argument("-i", "--image",nargs="+",required=True,
 	help="path to the input image")
 ap.add_argument("-c", "--cascade",
 	default="haarcascade_frontalcatface.xml",
@@ -16,23 +16,52 @@ args = vars(ap.parse_args())
 
 
 
-image = cv2.imread(args["image"])
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-detector = cv2.CascadeClassifier(args["cascade"])
-rects = detector.detectMultiScale(gray, scaleFactor=1.3,
-	minNeighbors=10, minSize=(75, 75))
-
-print(image)
-print(rects)
+print(args["image"][0])
+# print(type(args["image"][1]))
+print(len(args["image"]))
 
 
-os.chdir("yeni")
-if rects!=():
-# 	# while True:
+for i in range (len(args["image"])):
+	image = cv2.imread(args["image"][i])
+	# print(type(image))
+	# print(image)
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	detector = cv2.CascadeClassifier(args["cascade"])
+	rects = detector.detectMultiScale(gray, scaleFactor=1.3,
+		minNeighbors=10, minSize=(75, 75))
 
-	cv2.imwrite('irem4.jpg',image)
+
+	os.chdir("yeni")
+
+	if rects!=():
 		
+		# image_reshaped=image.reshape(image.shape[0],-1)
+		# print(len(image_reshaped))
+		# cv2.imshow("cat",image_reshaped)
+		# cv2.waitKey(0)
 
+		# np.savetxt("yeni",image_reshaped )
+		# loaded_arr = np.loadtxt("yeni")
+		# data=im.fromarray(loaded_arr)
+
+		# if data.mode!='RGB':
+		# 	data=data.convert('RGB')
+		
+		# 	data.save('yeni7.jpg')
+
+		cv2.imwrite("{}.jpg".format(i),image)
+	
+
+
+		# array=np.reshape(image_reshaped,(224,224))
+
+		# data=im.fromarray(array)
+		# data.save('yeni.jpg')
+
+
+
+# print(type(image))
+# print(rects)
 
 
 
